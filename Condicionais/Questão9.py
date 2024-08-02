bc24ad494753ff1db3e77bb73d259e0e538eb90a
Epo-ky <evando.pereirali33@gmail.com>
2024-08-02 14:12:56 -0300

    Create Questão9.py

---
+++ Condicionais/Questão9.py
@@ -0,0 +1,2 @@
+x, op, y = int(input()),input(),int(input())
+print(x+y) if op =="+" else print(x-y)if op == "-" else print(x*y) if op =="*" else print(x//y) if op == "/" else print("Erro: operador não reconhecido.")
